7612ed5db84b923f8c6f62124160a089e2669252
Daisy <daisykuria7@gmail.com>
2023-08-24 18:10:00 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: This function demonstrates how to modify a value indirectly
- * using a pointer.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
 {
 int n;
 int a[5];
